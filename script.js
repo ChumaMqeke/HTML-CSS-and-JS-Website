5ee0a81abca92f9972644b4f00b5394a2692533e
Chuma Mqeke <cmqeke2401@gmail.com>
2023-09-19 14:31:47 +0200

    game

--- script.js
+++ script.js
@@ -19,7 +19,46 @@ window.scroll = () => {
 }
 
 
-
+ // Get references to HTML elements
+ const textToTypeElement = document.getElementById("textToType");
+ const userInputElement = document.getElementById("userInput");
+ const scoreElement = document.getElementById("score");
+ 
+ let score = 0;
+ 
+ // Generate initial random text
+ let currentText = generateRandomText();
+ textToTypeElement.textContent = currentText;
+ 
+ // Function to generate random text
+ function generateRandomText() {
+     const texts = ["This is a sample text.", "Type this quickly.", "Keep typing!", "Test your skills."];
+     const randomIndex = Math.floor(Math.random() * texts.length);
+     return texts[randomIndex];
+ }
+ 
+ // Function to update the score
+ function updateScore() {
+     score++;
+     scoreElement.textContent = score;
+ }
+ 
+ // Event listener for user input
+ userInputElement.addEventListener("input", function () {
+     const userInput = userInputElement.value;
+     
+     if (currentText.startsWith(userInput)) {
+         if (currentText === userInput) {
+             // User successfully typed the current text
+             updateScore();
+             currentText = generateRandomText();
+             textToTypeElement.textContent = currentText;
+             userInputElement.value = "";
+         }
+     } else {
+         // User made a mistake, you can handle this as needed
+     }
+ });
         
         
   
