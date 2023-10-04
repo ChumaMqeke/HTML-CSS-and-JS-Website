7fbcc43f1d3b238ec42be9a4eecd68d729cdfebf
SIWEPEKO01 <141214506+SIWEPEKO01@users.noreply.github.com>
2023-10-04 09:01:30 +0200

    iframe, cookies, arrayBuffer

--- script.js
+++ script.js
@@ -206,3 +206,41 @@ function resetValues() {
   cpm_group.style.display = "none";
   wpm_group.style.display = "none";
 }
+
+
+
+function myFunction() {
+  let text;
+  if (confirm("Would you like to improve your typing skill") == true) {
+   document.getElementById("game").display.stlye = "block";
+  } else {
+    document.getElementById("game").display.stlye = "none";
+  }
+  
+}
+
+//cookies
+
+const cookieBox = document.querySelector(".wrapper"),
+  buttons = document.querySelectorAll(".button");
+
+const executeCodes = () => {
+  //if cookie contains codinglab it will be returned and below of this code will not run
+  if (document.cookie.includes("codinglab")) return;
+  cookieBox.classList.add("show");
+
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      cookieBox.classList.remove("show");
+
+      //if button has acceptBtn id
+      if (button.id == "acceptBtn") {
+        //set cookies for 1 month. 60 = 1 min, 60 = 1 hours, 24 = 1 day, 30 = 30 days
+        document.cookie = "cookieBy= codinglab; max-age=" + 10 * 0 * 0 * 0;
+      }
+    });
+  });
+};
+
+//executeCodes function will be called on webpage load
+window.addEventListener("load", executeCodes);
